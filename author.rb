b9826caabeb60ae2e53539743ac6e62c9e94964b
brhanuhailu <hailubrhanu29@gmail.com>
2023-05-30 02:38:51 -0700

    authors

---
+++ author.rb
@@ -0,0 +1,22 @@
+require_relative 'item'
+
+class Author
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(first_name, last_name, _item)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def add_author(item)
+    @items.push(item)
+    item.author = self
+  end
+end
