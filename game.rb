7b5fbedd69ea5a5e31474e582227bfaa7d24c091
Brhanu Hailu <hailubrhanu29@gmail.com>
2023-05-30 12:43:29 -0700

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,4 +1,3 @@
-
 require_relative 'item'
 require 'date'
 
@@ -23,26 +22,5 @@ class Game < Item
   def can_be_archived?
     age_in_years = Time.now.year - @last_played_at.year
     age_in_years >= 2
-  end
-end
-
-class Game < Item
-  attr_accessor :multiplayer, :last_played_date
-
-  def initialize(genre, author, published_date, multiplayer, last_played_date)
-    super(genre, author, label, published_date)
-    @multiplayer = multiplayer
-    @last_played_date = last_played_date
-  end
-
-  def move_to_archive
-    @archived = true if can_be_archived?
-  end
-
-  private
-
-  def can_be_archived?
-    age_in_years = Time.now.year - @last_played_date.year
-    age_in_years >= 10
   end
 end
