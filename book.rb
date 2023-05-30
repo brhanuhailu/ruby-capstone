9190cb1202d3202818617c682c32a65ad975825e
yodit93 <yoditabebe9391@gmail.com>
2023-05-30 17:47:58 +0300

    Update book label property

--- book.rb
+++ book.rb
@@ -1,8 +1,9 @@
 require_relative 'item'
 
 class Book < Item
-  def initialize(genre, author, published_date, publisher, cover_state)
-    super(genre, author, published_date)
+    attr_accessor :publisher, :cover_state
+  def initialize(genre, author, published_date, publisher, cover_state, label = nil)
+    super(genre, author, published_date, label)
     @publisher = publisher
     @cover_state = cover_state
   end
