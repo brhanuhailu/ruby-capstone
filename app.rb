864675a9644941d0138283777290905798f5a657
yodit93 <yoditabebe9391@gmail.com>
2023-05-30 14:37:14 +0300

    Add methods for book and label classes

--- app.rb
+++ app.rb
@@ -1,7 +1,52 @@
+require_relative 'book'
+require_relative 'label'
+
 class App
-  def iniliaze
+  def initialize
     @books = []
-    @people = []
-    @rentals = []
+    @labels = []
+  end
+
+  def list_books
+    @books.each do |book|
+      puts "#{book.id} - #{book.genre} - #{book.author} - #{book.published_date}"
+    end
+  end
+
+  def list_labels
+    @labels.each do |label|
+      puts "#{label.id} - #{label.title} - #{label.color}"
+    end
+  end
+
+  def create_book
+    puts 'Enter genre'
+    genre = gets.chomp
+    puts 'Enter author'
+    author = gets.chomp
+    puts 'Enter published date (YYYY-MM-DD))'
+    published_date = gets.chomp
+    puts 'Enter publisher'
+    publisher = gets.chomp
+    puts 'Enter cover state'
+    cover_state = gets.chomp
+    book = Book.new(genre, author, published_date, publisher, cover_state)
+    @books << book
+    puts 'Book created successfully'
+  end
+
+  def create_label
+    puts 'Enter title'
+    title = gets.chomp
+    puts 'Enter color'
+    color = gets.chomp
+    label = Label.new(title, color)
+    @labels << label
+    puts 'Label created successfully'
+  end
+
+  def exit_app
+    puts 'Thanks for using the app!'
+    exit
   end
 end
