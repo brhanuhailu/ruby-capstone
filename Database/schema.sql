39b1d7f5f11dcd3ede034bbe25618c2d06ad777c
yodit93 <yoditabebe9391@gmail.com>
2023-05-31 12:37:57 +0300

    Create book table

--- Database/schema.sql
+++ Database/schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE label (
     title VARCHAR(100) NOT NULL,
     color VARCHAR(100) NOT NULL
 );
+
+-- create book table
+CREATE TABLE book (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    author TEXT,
+    published_date DATE,
+    publisher TEXT,
+    archived BOOLEAN NOT NULL DEFAULT FALSE,
+    cover_state VARCHAR(5),
+    label_id int REFERENCES label(id),
+    genre TEXT
+);
+
