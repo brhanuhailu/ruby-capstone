6c6f8fa884dbb0102004faab8a6bf80482f2cd3b
yodit93 <yoditabebe9391@gmail.com>
2023-05-31 12:37:28 +0300

    Create label table

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,3 +1,10 @@
 -- create catalog of my things database
 CREATE DATABASE catalog;
 
+-- create label table
+
+CREATE TABLE label (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    color VARCHAR(100) NOT NULL
+);
